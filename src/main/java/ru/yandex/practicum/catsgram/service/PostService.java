9df9abb2df34b168c18695e60587864ea1d12291
Vyacheslav <torlant312@yandex.ru>
2023-03-04 15:57:01 +0300

    feat: добавить проверку пользователя в PostService()

--- src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
+++ src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
@@ -1,20 +1,33 @@
 package ru.yandex.practicum.catsgram.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.catsgram.exceptions.UserNotFoundException;
 import ru.yandex.practicum.catsgram.model.Post;
+import ru.yandex.practicum.catsgram.model.User;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class PostService {
+    private final UserService userService;
     private final List<Post> posts = new ArrayList<>();
+
+    @Autowired
+    public PostService(UserService userService) {
+        this.userService = userService;
+    }
 
     public List<Post> findAll() {
         return posts;
     }
 
     public Post create(Post post) {
+        User user = userService.findUserByEmail(post.getAuthor());
+        if (user == null) {
+            throw new UserNotFoundException("Пользователь " + post.getAuthor() + " не найден");
+        }
         posts.add(post);
         return post;
     }
